036c250e835697e2acf4cd0c78cb9eda3e5a39d8
juneglee <klcpop1@gmail.com>
2022-08-30 17:13:57 +0900

    [add] deep convolutional GAN

---
+++ dc_gan.py
@@ -0,0 +1,252 @@
+import argparse
+import os
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+# 데이터셋의 경로
+dataroot = '../data/'
+
+# dataloader에서 사용할 쓰레드 수
+workers = 0
+
+# 배치 크기
+batch_size = 128
+
+# 이미지의 크기입니다. 모든 이미지들은 transformer를 이용해 64로 크기가 통일됩니다.
+image_size = 28
+
+# 이미지의 채널 수로, RGB 이미지이기 때문에 3으로 설정합니다.
+nc = 1 # 1
+
+# 잠재공간 벡터의 크기 (예. 생성자의 입력값 크기)
+nz = 100
+
+# 생성자를 통과하는 특징 데이터들의 채널 크기
+ngf = 64
+
+# 구분자를 통과하는 특징 데이터들의 채널 크기
+ndf = 64
+
+# 학습할 에폭 수
+num_epochs = 5
+
+# 옵티마이저의 학습률
+lr = 0.0002
+
+# Adam 옵티마이저의 beta1 하이퍼파라미터
+beta1 = 0.5
+
+# 사용가능한 gpu 번호. CPU를 사용해야 하는경우 0으로 설정하세요
+ngpu = 0
+
+transforms = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.CenterCrop(image_size),
+    transforms.RandomCrop(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+dataloader = DataLoader(
+    datasets.MNIST(root=dataroot, transform=transforms, download=True),
+    batch_size=batch_size, shuffle=True)
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else 'cpu')
+
+# netG와 netD에 적용시킬 커스텀 가중치 초기화 함수
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class Generator(nn.Module):
+    def __init__(self, ngpu, nc=1, nz=100, ngf=64):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(    ngf,      nc, kernel_size=1, stride=1, padding=2, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
+netG = Generator(ngpu).to(device)
+netG.apply(weights_init)
+print(netG)
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu, nc=1, ndf=64):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, 1, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        if input.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output.view(-1, 1).squeeze(1)
+
+netD = Discriminator(ngpu).to(device)
+netD.apply(weights_init)
+print(netD)
+
+criterion = nn.BCELoss()
+
+# 생성자의 학습상태를 확인할 잠재 공간 벡터를 생성합니다
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+# 학습에 사용되는 참/거짓의 라벨을 정합니다
+real_label = 1.
+fake_label = 0.
+
+# G와 D에서 사용할 Adam옵티마이저를 생성합니다
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+fake_images = []
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop ..")
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader, 0):
+        ############################
+        # (1) D 신경망을 업데이트 합니다: log(D(x)) + log(1 - D(G(z)))를 최대화 합니다
+        ###########################
+        ## 진짜 데이터들로 학습을 합니다
+        netD.zero_grad()
+        # 배치들의 사이즈나 사용할 디바이스에 맞게 조정합니다
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label,
+                           dtype=torch.float, device=device)
+        # 진짜 데이터들로 이루어진 배치를 D에 통과시킵니다
+        output = netD(real_cpu).view(-1)
+        # 손실값을 구합니다
+        errD_real = criterion(output, label)
+        # 역전파의 과정에서 변화도를 계산합니다
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## 가짜 데이터들로 학습을 합니다
+        # 생성자에 사용할 잠재공간 벡터를 생성합니다
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # G를 이용해 가짜 이미지를 생성합니다
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # D를 이용해 데이터의 진위를 판별합니다
+        output = netD(fake.detach()).view(-1)
+        # D의 손실값을 계산합니다
+        errD_fake = criterion(output, label)
+        # 역전파를 통해 변화도를 계산합니다. 이때 앞서 구한 변화도에 더합니다(accumulate)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # 가짜 이미지와 진짜 이미지 모두에서 구한 손실값들을 더합니다
+        # 이때 errD는 역전파에서 사용되지 않고, 이후 학습 상태를 리포팅(reporting)할 때 사용합니다
+        errD = errD_real + errD_fake
+        # D를 업데이트 합니다
+        optimizerD.step()
+
+        ############################
+        # (2) G 신경망을 업데이트 합니다: log(D(G(z)))를 최대화 합니다
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # 생성자의 손실값을 구하기 위해 진짜 라벨을 이용할 겁니다
+        # 우리는 방금 D를 업데이트했기 때문에, D에 다시 가짜 데이터를 통과시킵니다.
+        # 이때 G는 업데이트되지 않았지만, D가 업데이트 되었기 때문에 앞선 손실값가 다른 값이 나오게 됩니다
+        output = netD(fake).view(-1)
+        # G의 손실값을 구합니다
+        errG = criterion(output, label)
+        # G의 변화도를 계산합니다
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # G를 업데이트 합니다
+        optimizerG.step()
+
+        print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+              % (epoch, num_epochs, i, len(dataloader),
+                 errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # 이후 그래프를 그리기 위해 손실값들을 저장해둡니다
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # fixed_noise를 통과시킨 G의 출력값을 저장해둡니다
+        batch_done = epoch * len(dataloader) + i
+        if (iters % 100 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            fake_images.append(fake)
+            vutils.save_image(fake_images[-1], "images/%d.png" % batch_done,  normalize=True)
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
+
+    # model save
+    model_dir = "models"
+    os.makedirs(model_dir, exist_ok=True)
+
+    torch.save({
+        'num_epoch': num_epochs,
+        'model_G_state_dict' : netG.state_dict(),
+        'model_D_state_dict': netD.state_dict(),
+        'optimizer_G_state_dict': optimizerG.state_dict(),
+        'optimizer_D_state_dict': optimizerD.state_dict(),
+    },os.path.join(model_dir, 'dcgan_mnist_%d.pt' % (epoch)))
+    print(f'GAN Model checkpoint saved {epoch}.. ')
