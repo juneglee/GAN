86b6c75c4b0c8a6fed24170328765b7db9959982
juneglee <klcpop1@gmail.com>
2022-08-31 16:52:28 +0900

    [mod] deep convolutional GAN

--- dc_gan.py
+++ dc_gan.py
@@ -67,14 +67,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-# custom weights initialization called on netG and netD
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
 
 class Generator(nn.Module):
     def __init__(self, ngpu, nc=1, nz=100, ngf=64):
